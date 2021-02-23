7423d373890a01b8d771c110aef760bfc4ace2a9
Jevgenij Golobokin <jevgenij.golobokin@gmail.com>
2021-02-23 10:54:22 +0200

    initialize spring boot app

---
+++ src/main/java/com/ibm/intern/covid19statsapp/Covid19StatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.intern.covid19statsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19StatsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19StatsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibm/intern/covid19statsapp/Covid19StatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.intern.covid19statsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19StatsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
